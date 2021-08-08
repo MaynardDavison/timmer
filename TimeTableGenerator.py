94fa6803f0b235458643e347df38d7bac7f63b33
Maynard Davison <youhan3041@163.com>
2021-08-08 18:02:56 +0800

    测试

---
+++ TimeTableGenerator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/7 18:04
+# @Author  : hanyou
+# @Software: PyCharm
+
+
+def a(b,c):
+    """
+    test
+
+    Args:
+        b(int): test2
+        c(float):test3
+
+    Returns: None
+
+    """
+    b=1
+    c=2
+
+
